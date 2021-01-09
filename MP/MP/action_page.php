7eda32c713ba5b0276667f702d0283dd5f4efbfa
shraddha nahar <shraddhanahar7@gmail.com>
2021-01-10 02:50:55 +0530

    Almost complete --linked

---
+++ MP/MP/action_page.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>The name Attribute</h2>
+
+<form action="/action_page.php">
+  <input type="text" placeholder="Search.." name="search">
+  <input type="submit" value="Submit">
+</form> 
+
+<p>If you click the "Submit" button, the form-data will be sent to a page called "/action_page.php".</p>
+
+<p>Notice that the value of the "First name" field will not be submitted, because the input element does not have a name attribute.</p>
+
+</body>
+</html>
